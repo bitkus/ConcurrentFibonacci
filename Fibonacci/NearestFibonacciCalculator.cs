1a0e7aee0acb2d3a349902e514dc94479db72686
Linas Bitkevičius <linas.bitkevicius@gmail.com>
2020-01-27 16:40:43 +0200

    Initial commit

---
+++ Fibonacci/NearestFibonacciCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    public class NearestFibonacciCalculator
+    {
+        public long GetNearestFib(long number)
+        {
+            var positive = GetNearestFibPositive(number);
+            var negative = GetNearestFibNegative(number);
+            return positive - number > number - negative ? negative : positive;
+        }
+
+        private long GetNearestFibNegative(long number)
+        {
+            return GetNearestFib(number, -1);
+        }
+
+        private long GetNearestFibPositive(long number)
+        {
+            return GetNearestFib(number, 1);
+        }
+
+        private long GetNearestFib(long number, int increment)
+        {
+            var candidate = number;
+            var isFib = false;
+
+            do
+            {
+                isFib = IsFib(candidate);
+                if (!isFib)
+                {
+                    candidate += increment;
+                }
+            } while (!isFib);
+
+            return candidate;
+        }
+
+        private bool IsFib(long number)
+        {
+            return IsWholeNumberWithPrecision(Math.Sqrt(5 * number * number + 4)) || IsWholeNumberWithPrecision(Math.Sqrt(5 * number * number - 4));
+        }
+
+        private bool IsWholeNumberWithPrecision(double number)
+        {
+            return Math.Abs(number % 1) <= Double.Epsilon;
+        }
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        private static readonly FibonacciState State = new FibonacciState();
+
+        static void Main(string[] args)
+        {
+            var nearestFibonacciCalculator = new NearestFibonacciCalculator();
+            var fibonacciSequenceCalculator = new FibonacciSequenceCalculator();
+            Console.WriteLine(nearestFibonacciCalculator.GetNearestFib(11));
+
+            for (int i = 0; i < 10; i++)
+            {
+                var fib = fibonacciSequenceCalculator.GetNext(State);
+                UpdateState(fib);
+                Console.WriteLine(fib.Value);
+            }
+        }
+
+        private static void UpdateState(KeyValuePair<int, long> fib)
+        {
+            State.Add(fib.Key, fib.Value);
+        }
+    }
+}
